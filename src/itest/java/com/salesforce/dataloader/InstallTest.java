57e018e0bb127313f933cb6f54e8ea7b26d5c525
Jeff Lai <jeffrey.lai@salesforce.com>
2012-01-26 15:44:30 -0800

    installer test

---
+++ src/itest/java/com/salesforce/dataloader/InstallTest.java
@@ -0,0 +1,29 @@
+package com.salesforce.dataloader;
+
+import java.io.File;
+
+import autoitx4java.AutoItX;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.jacob.com.LibraryLoader;
+
+public class InstallTest {
+    
+    @BeforeClass
+    public void classSetup() {
+        File dll = new File(System.getProperty("user.home") + File.pathSeparator + ".m2"
+               + File.pathSeparator + "repository" + File.pathSeparator + "net" + File.pathSeparator + "sf"
+               + File.pathSeparator + "jacob-project" + File.pathSeparator + "jacob" + File.pathSeparator + "1.14.3"
+               + File.pathSeparator + "jacob-1.14.3-x86.dll");
+        System.setProperty(LibraryLoader.JACOB_DLL_PATH, dll.getAbsolutePath());
+    }
+    
+    @Test
+    public void testRunInstaller() {
+        AutoItX autoIt = new AutoItX();
+        autoIt.run("notepad.exe");
+    }
+
+}
