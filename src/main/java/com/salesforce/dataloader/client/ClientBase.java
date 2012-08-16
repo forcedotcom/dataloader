d1b486d1dc5c0f508724ccb27811a61981a92383
Jeff Lai <jeffrey.lai@salesforce.com>
2012-08-15 21:05:06 -0700

    throw runtime exception in HttpClientTransport only for 407 proxy auth required error

--- src/main/java/com/salesforce/dataloader/client/ClientBase.java
+++ src/main/java/com/salesforce/dataloader/client/ClientBase.java
@@ -101,6 +101,7 @@ public abstract class ClientBase<ClientType> {
         ConnectorConfig cc = new ConnectorConfig();
         cc.setTransport(HttpClientTransport.class);
         cc.setSessionId(getSessionId());
+        
         // set authentication credentials
         // blank username is not acceptible
         String username = config.getString(Config.USERNAME);
