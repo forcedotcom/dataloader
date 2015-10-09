8272e36bc4e406cb9b58d4b8d9c3df5fed67f4e4
Rocky <rmazzeo@salesforce.com>
2015-10-08 18:04:25 -0700

    Housecleaning with the pom and the mac installer
    Revised test run list to exclude test which require connectivity. Need
    a solution to run these without that doesn’t confuse and frustrate.
    Also, the mac installer was spazzing out for a few devs. Tickled it a
    bit.

--- make-pretty-dmg.sh
+++ make-pretty-dmg.sh
@@ -2,20 +2,16 @@
 
 BASEDIR="$1"
 DMG_SRC_DIR="$2"
-DMG_OUT="$3"
-CODE_SIGN="$4"
+TIME=$(date +%s)
+DMG_OUT="${BASEDIR}/target/ApexDataLoader.${TIME}.dmg"
+CODE_SIGN="$3"
 
 chflags nouchg $DMG_SRC_DIR/*
 chmod -R +w $DMG_SRC_DIR
 
-if [ "$CODE_SIGN" -ne "DONTSIGN" ]
+if [ "$CODE_SIGN" != "DONTSIGN" ]
 then
     codesign --force --verify --sign "$4" $DMG_SRC_DIR/Data\ Loader.app
 fi
 
-bash $BASEDIR/yoursway-create-dmg/create-dmg --volname "Data Loader" --background "$BASEDIR/src/main/resources/img/installscreens.gif" --hide-extension "Data Loader.app" --icon-size 60 --window-size 400 300 --app-drop-link 300 230 --icon "Data Loader" 100 230 "$DMG_OUT" $DMG_SRC_DIR
-
-if [ "$CODE_SIGN" -ne "DONTSIGN" ]
-then
-    codesign --force --verify --sign "$4" $DMG_OUT
-fi
+bash $BASEDIR/yoursway-create-dmg/create-dmg --volname "DataLoader" --background "$BASEDIR/src/main/resources/img/installscreens.gif" --hide-extension "Data Loader.app" --icon-size 60 --window-size 400 300 --app-drop-link 300 230 --icon "$BASEDIR/src/main/resources/img/icons/icon_loader.icns" 100 230 "$DMG_OUT" $DMG_SRC_DIR
