af5f6a3ee3b7b4b5855b5cec761b8dbde09f3fe3
rocky j mazzeo <rjmazzeo@hotmail.com>
2016-02-02 14:40:57 -0800

    Fie

--- src/main/nsis/bin/encrypt.bat
+++ src/main/nsis/bin/encrypt.bat
@@ -1,3 +1,23 @@
 @echo off
 
-..\Java\bin\java.exe -cp ..\${pom.build.finalName}-uber.jar com.salesforce.dataloader.security.EncryptionUtil %*
+IF "%JAVA_HOME%" == "" (
+    for /f "tokens=*" %%i in ('${pom.build.finalName}-java-home.exe') do (
+        IF EXIST "%%i" (
+            set JAVA_HOME=%%i
+        ) ELSE (
+            echo %%i
+        )
+    )
+)
+
+IF "%JAVA_HOME%" == "" (
+    echo Please set JAVA_HOME before running the encrypt.bat
+) ELSE (
+    IF NOT EXIST "%JAVA_HOME%" (
+        echo Could not find "%JAVA_HOME%"
+    ) ELSE (
+        "%JAVA_HOME%\bin\java"  -cp ..\${pom.build.finalName}-uber.jar com.salesforce.dataloader.security.EncryptionUtil %*
+    )
+)
+
+
