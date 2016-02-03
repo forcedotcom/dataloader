a0f9c5e980c25cdda14298b05b5b719b9f5cddd7
Rocky <rmazzeo@salesforce.com>
2016-02-03 12:58:47 -0800

    Final updated text for batch commands

--- src/main/nsis/bin/encrypt.bat
+++ src/main/nsis/bin/encrypt.bat
@@ -11,10 +11,10 @@ IF "%JAVA_HOME%" == "" (
 )
 
 IF "%JAVA_HOME%" == "" (
-    echo Please set JAVA_HOME before running the encrypt.bat
+    echo To run encrypt.bat, set the JAVA_HOME environment variable to the directory where the Java Runtime Environment (JRE) is installed.
 ) ELSE (
     IF NOT EXIST "%JAVA_HOME%" (
-        echo Could not find "%JAVA_HOME%"
+        echo We couldn't find the Java Runtime Environment (JRE) in directory %JAVA_HOME%. To run process.bat, set the JAVA_HOME environment variable to the directory where the JRE is installed.
     ) ELSE (
         "%JAVA_HOME%\bin\java"  -cp ..\${pom.build.finalName}-uber.jar com.salesforce.dataloader.security.EncryptionUtil %*
     )
--- src/main/nsis/bin/process.bat
+++ src/main/nsis/bin/process.bat
@@ -36,10 +36,10 @@ IF "%JAVA_HOME%" == "" (
 )
 
 IF "%JAVA_HOME%" == "" (
-    echo Please set JAVA_HOME before running the encrypt.bat
+    echo To run process.bat, set the JAVA_HOME environment variable to the directory where the Java Runtime Environment (JRE) is installed.
 ) ELSE (
     IF NOT EXIST "%JAVA_HOME%" (
-        echo Could not find "%JAVA_HOME%"
+        echo We couldn't find the Java Runtime Environment (JRE) in directory %JAVA_HOME%. To run process.bat, set the JAVA_HOME environment variable to the directory where the JRE is installed.
     ) ELSE (
         "%JAVA_HOME%\bin\java" -cp ..\${pom.build.finalName}-uber.jar -Dsalesforce.config.dir=%1 com.salesforce.dataloader.process.ProcessRunner %PROCESS_OPTION%
     )
