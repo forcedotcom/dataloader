d0c3cd85c7abadc5d105594a1c9162aed49ed7c2
brytonpilling <frozenmist@gmail.com>
2020-01-30 08:08:26 +1300

    Updated length of proxyPort field on ExtractionInputDialog
    Updated the length of the proxy port field in ExtractionInputDialog to allow proxies that have ports greater than 4 digits. Now allow 5 digit port numbers.

--- src/main/java/com/salesforce/dataloader/ui/extraction/ExtractionInputDialog.java
+++ src/main/java/com/salesforce/dataloader/ui/extraction/ExtractionInputDialog.java
@@ -285,7 +285,7 @@ public class ExtractionInputDialog extends Dialog {
 
         textProxyPort = new Text(restComp, SWT.BORDER);
         textProxyPort.setText(config.getString(Config.PROXY_PORT));
-        textProxyPort.setTextLimit(4);
+        textProxyPort.setTextLimit(5);
         textProxyPort.addVerifyListener(new VerifyListener() {
             @Override
             public void verifyText(VerifyEvent event) {
@@ -293,7 +293,7 @@ public class ExtractionInputDialog extends Dialog {
             }
         });
         data = new GridData();
-        data.widthHint = 25;
+        data.widthHint = 30;
         textProxyPort.setLayoutData(data);
 
         //Proxy Username
