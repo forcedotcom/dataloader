7517541f6986ca0a48d094185c458db499677b98
roeschter <michael@roeschter.de>
2020-10-20 14:47:21 +0200

    Update dataloader.bat
    Numerous improvement as the previous version would fail to detect Zulu 8 and would not work with multiple Java installations in the PATH 
    - Allow for override
    	- set ZULU_JAVA_HOME
    	- Check for ZULU_JAVA_HOME set 
    - Print java -version in start script
    - Check required version 
    - Cleaner failure messages

--- release/win/dataloader.bat
+++ release/win/dataloader.bat
@@ -19,19 +19,39 @@ echo **                                                                     **
 echo *************************************************************************
 echo.
 
+rem set ZULU_JAVA_HOME=D:\Java\zulu11_64\bin\
+if NOT "%ZULU_JAVA_HOME%"=="" (
+	echo Using '%ZULU_JAVA_HOME%'
+	echo Skipping Search
+)
+
 :CheckOpenJdk11
-    REM: Todo, we need to improve this to better detect JAVA 11+, rather than depending on default zulu installed folder.
-    echo Data Loader requires Zulu OpenJDK 11. Checking if it is installed...
-    for /F "delims=" %%a in ('powershell -Command "foreach($path in (Get-ChildItem Env:Path).value -split ';') { if($path -like '*zulu*') { $jdkDir = $path -split 'bin\\' }}; echo $jdkDir"') do Set "ZULU_JAVA_HOME=%%a"
-    if "%ZULU_JAVA_HOME%"=="" (
-        echo Zulu OpenJDK is not installed. Download Zulu OpenJDK 11 for Windows here:
-        echo    https://www.azul.com/downloads/zulu/zulu-windows/
-        PAUSE
-        goto Exit
-    )
+	echo Data Loader requires Zulu OpenJDK 11. Checking if it is installed...
+	
+	if "%ZULU_JAVA_HOME%"=="" (
+		echo Checking in PATH
+		for /F "delims=" %%a in ('powershell -Command "foreach($path in (Get-ChildItem Env:Path).value -split ';') { if($path -like '*zulu*') { echo $path }};"') do (
+			echo Found Zulu in: '%%a' - Checking version	
+			FOR /f "tokens=4 delims=. " %%t IN ('"%%a\java" -fullversion 2^>^&1' ) do (
+				if %%t==^"11 (
+					echo Version check suceeded for %%a
+					Set "ZULU_JAVA_HOME=%%a"
+				)
+				if NOT %%t==^"11 (
+					echo Version check failed
+				)
+			)
+		)
+	)
+	REM If still nothing found
+	if "%ZULU_JAVA_HOME%"=="" (
+		echo Zulu OpenJDK 11 not found. Download Zulu OpenJDK 11 for Windows here:
+		echo    https://www.azul.com/downloads/zulu/zulu-windows/
+		PAUSE
+		goto Exit
 
 :Run
-    "%ZULU_JAVA_HOME%\bin\java"  -jar dataloader-@@FULL_VERSION@@-uber.jar salesforce.config.dir=configs
+    "%ZULU_JAVA_HOME%\java"  -jar dataloader-@@FULL_VERSION@@-uber.jar salesforce.config.dir=configs
 
 :Exit
     endlocal
