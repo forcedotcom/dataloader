7342f6eadca37e2ee6c2d4eb0a26776b0675d7b9
Ashit Patel <ashit.patel@salesforce.com>
2021-03-13 20:35:03 -0800

    specify Timestamp Authority (TSA) when signing the jar
    Specifying the TSA when signing the jar avoids the warning "This jar contains signatures that do not include a timestamp."

--- dlbuilder.sh
+++ dlbuilder.sh
@@ -2,16 +2,17 @@
 
 # $1 - password for the java key store containing code-signing cert
 # $2 - file name of the java key store.
+# $3 - URL of the TSA (Timestamp Authority)
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 KeystorePassword KeystoreFile" >&2
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <Keystore Password> <Keystore File> <TSA URL>" >&2
   exit 1
 fi
 
-mvn clean package -DskipTests  -D"jarsigner.storepass=$1"  -D"jarsigner.keystore=$2" -D'jarsigner.skip=false' -D'jarsigner.alias=1' -Pzip
+mvn clean package -DskipTests -D"jarsigner.storepass=$1" -D"jarsigner.keystore=$2" -D"tsa.url=$3" -D'jarsigner.skip=false' -D'jarsigner.alias=1' -Pzip
 
 cp target/mac/dataloader_mac.zip .
 
-mvn clean package -DskipTests -D"jarsigner.storepass=$1" -D"jarsigner.keystore=$2" -D'jarsigner.skip=false' -D'jarsigner.alias=1' -Pwin64,zip,-mac64
+mvn clean package -DskipTests -D"jarsigner.storepass=$1" -D"jarsigner.keystore=$2" -D"tsa.url=$3" -D'jarsigner.skip=false' -D'jarsigner.alias=1' -Pwin64,zip,-mac64
 
 cp target/win/dataloader_win.zip .
