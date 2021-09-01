d7657668132f2d4fa435a69146c0b56df200ab6b
Ashit Patel <ashit.patel@salesforce.com>
2021-08-31 20:38:31 -0700

    update of dlbuilder.sh to support unzipped, os-specific builds
    update of dlbuilder.sh to support unzipped, os-specific builds

--- dlbuilder.sh
+++ dlbuilder.sh
@@ -1,45 +1,85 @@
 #!/bin/sh -f
-
 # $1 - password for the java key store containing code-signing cert
 # $2 - file name of the java key store.
 # $3 - URL of the TSA (Timestamp Authority)
 
+usage() {
+  echo "Usage: "
+  echo "$0 -u [-n <macos_x86_64 | macos_arm_64 | windows_x86_64 | linux_x86_64>]"
+  echo "$0 <Keystore Password> <Keystore File> <TSA URL>" >&2
+  exit 1
+}
+
+run_mvn() {
+# $1 - code-signing params
+# $2 - target OS name
+# $3 - true - zip, false - do not zip
+
+  if [ "$2" = macos_x86_64 ]
+  then
+    from=target/mac/dataloader_mac.zip
+    to=./dataloader_mac.zip
+  elif [ "$2" = macos_arm_64 ] 
+  then
+    from=target/mac/dataloader_mac.zip
+    to=./dataloader_mac_arm64.zip
+  elif [ "$2" = windows_x86_64 ] 
+  then
+    from=target/win/dataloader_win.zip
+    to=./dataloader_win.zip
+  elif [ "$2" = linux_x86_64 ] 
+  then
+    from=target/linux/dataloader_linux.zip
+    to=./dataloader_linux.zip
+  fi
+
+  zipOption=""
+  if [ $3 = true ]
+  then
+    zipOption="-Pzip"
+  fi
+  mvn clean package -DskipTests $1 -DtargetOS=$2 ${zipOption}
+  if [ $3 = true ]
+  then
+    cp ${from} ${to}
+  fi
+}
+
+#################
+
 unsignedArtifacts=false
-while getopts u flag
+doZip=true
+
+while getopts ":un:" flag
 do
     case "${flag}" in
-        u) unsignedArtifacts=true;;
+        u)
+          unsignedArtifacts=true
+          ;;
+        n) 
+          targetOS=${OPTARG}
+          doZip=false
+          ;;
+        *)
+          usage
+          ;;
     esac
 done
 
 if [ ${unsignedArtifacts} = false  -a  "$#" -ne 3 ]; then
-  echo "Usage: "
-  echo "$0 -u"
-  echo "$0 <Keystore Password> <Keystore File> <TSA URL>" >&2
-  exit 1
+  usage
 fi
 
-# build for macx86_64 platform
 signingOptions=
 if [ ${unsignedArtifacts} = false ]; then
   signingOptions="-Djarsigner.storepass=$1 -Djarsigner.keystore=$2 -Djarsigner.tsa=$3 -Djarsigner.skip=false -Djarsigner.alias=1"
 fi
 
-mvn clean package -DskipTests ${signingOptions} -DtargetOS=macos_x86_64 -Pzip
-
-cp target/mac/dataloader_mac.zip .
-
-# build for Mac ARM platform
-#mvn clean package -DskipTests ${signingOptions} -DtargetOS=macos_arm_64 -Pzip
-
-#cp target/mac/dataloader_mac.zip ./dataloader_mac_arm64.zip
-
-# build for Windows platform
-mvn clean package -DskipTests ${signingOptions} -DtargetOS=windows_x86_64 -Pzip
-
-cp target/win/dataloader_win.zip .
-
-# build for linux platform
-#mvn clean package -DskipTests ${signingOptions} -DtargetOS=linux_x86_64 -Pzip
-
-#cp target/linux/dataloader_linux.zip .
+if [ ${doZip} = true ]; then
+  run_mvn "${signingOptions}" "macos_x86_64" ${doZip}
+  run_mvn "${signingOptions}" "windows_x86_64" ${doZip}
+#  run_mvn "${signingOptions}" "macos_arm_64" ${doZip}
+#  run_mvn "${signingOptions}" "linux_x86_64" ${doZip}
+else
+  run_mvn "${signingOptions}" "${targetOS}" ${doZip}
+fi
