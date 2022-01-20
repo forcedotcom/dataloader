ef630eebe29610677dc1ff505eefc678b05386bd
ashitsalesforce <ashit.patel@salesforce.com>
2022-01-20 15:24:32 -0800

    use the term "batch process bean id"
    use the term "batch process bean id" instead of "process name" or "operation" or something else.

--- release/win/bin/process.bat
+++ release/win/bin/process.bat
@@ -1,12 +1,12 @@
 @echo off
 if not [%1]==[] goto run
 echo.
-echo Usage: process ^<configuration directory^> ^[process bean id^]
+echo Usage: process ^<configuration directory^> ^[batch process bean id^]
 echo.
 echo      configuration directory -- directory that contains configuration files,
 echo          i.e. config.properties, process-conf.xml, database-conf.xml
 echo.
-echo      process bean id -- optional bean id of a batch process bean in process-conf.xml,
+echo      batch process bean id -- optional bean id of a batch process bean in process-conf.xml,
 echo          for example:
 echo.
 echo              process ../myconfigdir AccountInsert
@@ -24,8 +24,8 @@ goto end
 set EXE_PATH=%~dp0
 set DATALOADER_VERSION=@@FULL_VERSION@@
 
-set PROCESS_BEAN_ID_OPTION=
-if not [%2]==[] set PROCESS_BEAN_ID_OPTION=process.name=%2
+set BATCH_PROCESS_BEAN_ID_OPTION=
+if not [%2]==[] set BATCH_PROCESS_BEAN_ID_OPTION=process.name=%2
 
 IF NOT "%DATALOADER_JAVA_HOME%" == "" (
     set JAVA_HOME="%DATALOADER_JAVA_HOME%"
@@ -37,7 +37,7 @@ IF "%JAVA_HOME%" == "" (
     IF NOT EXIST "%JAVA_HOME%" (
         echo We couldn't find the Java Runtime Environment ^(JRE^) in directory "%JAVA_HOME%". To run process.bat, set the JAVA_HOME environment variable to the directory where the JRE is installed.
     ) ELSE (
-        "%JAVA_HOME%\bin\java" -jar "%EXE_PATH%\..\dataloader-%DATALOADER_VERSION%-uber.jar" run.mode=batch salesforce.config.dir=%1 %PROCESS_BEAN_ID_OPTION%
+        "%JAVA_HOME%\bin\java" -jar "%EXE_PATH%\..\dataloader-%DATALOADER_VERSION%-uber.jar" run.mode=batch salesforce.config.dir=%1 %BATCH_PROCESS_BEAN_ID_OPTION%
     )
 )
 
