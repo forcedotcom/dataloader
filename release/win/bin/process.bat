493736a1e1b3af5bcfbed2eee3b8c8aa8fce7ca8
ashitsalesforce <ashit.patel@salesforce.com>
2022-01-20 15:15:19 -0800

    use -jar instead of -cp to avoid explicit specification of the class with main method.
    use -jar instead of -cp to avoid explicit specification of the class with main method.

--- release/win/bin/process.bat
+++ release/win/bin/process.bat
@@ -11,7 +11,7 @@ echo          for example:
 echo.
 echo              process ../myconfigdir AccountInsert
 echo.
-echo          If process name is not specified, the parameter values from config.properties
+echo          If process bean id is not specified, the value of the property process.name in config.properties
 echo          will be used to run the process instead of process-conf.xml,
 echo          for example:
 echo.
@@ -24,8 +24,8 @@ goto end
 set EXE_PATH=%~dp0
 set DATALOADER_VERSION=@@FULL_VERSION@@
 
-set PROCESS_OPTION=
-if not [%2]==[] set PROCESS_OPTION=process.name=%2
+set PROCESS_BEAN_ID_OPTION=
+if not [%2]==[] set PROCESS_BEAN_ID_OPTION=process.name=%2
 
 IF NOT "%DATALOADER_JAVA_HOME%" == "" (
     set JAVA_HOME="%DATALOADER_JAVA_HOME%"
@@ -37,7 +37,7 @@ IF "%JAVA_HOME%" == "" (
     IF NOT EXIST "%JAVA_HOME%" (
         echo We couldn't find the Java Runtime Environment ^(JRE^) in directory "%JAVA_HOME%". To run process.bat, set the JAVA_HOME environment variable to the directory where the JRE is installed.
     ) ELSE (
-        "%JAVA_HOME%\bin\java" -cp "%EXE_PATH%\..\dataloader-%DATALOADER_VERSION%-uber.jar" com.salesforce.dataloader.process.DataLoaderRunner salesforce.config.dir=%1 run.mode=batch %PROCESS_OPTION%
+        "%JAVA_HOME%\bin\java" -jar "%EXE_PATH%\..\dataloader-%DATALOADER_VERSION%-uber.jar" run.mode=batch salesforce.config.dir=%1 %PROCESS_BEAN_ID_OPTION%
     )
 )
 
