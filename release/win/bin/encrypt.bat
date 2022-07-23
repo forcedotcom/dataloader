1ea958d85ba56a3514b1726e7fcc33d7b9975343
ashitsalesforce <ashit.patel@salesforce.com>
2022-07-23 09:25:32 -0700

    minor cleanup of batch scripts
    - call endlocal at end of the script.
    - remove double-quotes when setting a variable with value of another variable.

--- release/win/bin/encrypt.bat
+++ release/win/bin/encrypt.bat
@@ -5,12 +5,13 @@ set EXE_PATH=%~dp0
 set DATALOADER_VERSION=@@FULL_VERSION@@
 
 IF NOT "%DATALOADER_JAVA_HOME%" == "" (
-    set JAVA_HOME="%DATALOADER_JAVA_HOME%"
+    set JAVA_HOME=%DATALOADER_JAVA_HOME%
 )
 
-IF [%JAVA_HOME%] == [] (
+IF "%JAVA_HOME%" == "" (
     echo To run encrypt.bat, set the JAVA_HOME environment variable to the directory where the Java Runtime Environment ^(JRE^) is installed.
 ) ELSE (
-    PATH="%JAVA_HOME%"\bin\;%PATH%;
+    PATH=%JAVA_HOME%\bin\;%PATH%;
     java -cp "%EXE_PATH%\..\dataloader-%DATALOADER_VERSION%-uber.jar" com.salesforce.dataloader.security.EncryptionUtil %*
 )
+endlocal
