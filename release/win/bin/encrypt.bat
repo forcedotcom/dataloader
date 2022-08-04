81f4a4b022aa433c55580d4b0c2322172a8e1503
ashitsalesforce <ashit.patel@salesforce.com>
2022-08-04 10:19:01 -0700

    bat script changes
    - Calling script may or may not decide to pause if java version check fails.
    - Changes to the displayed error message when java version check fails.

--- release/win/bin/encrypt.bat
+++ release/win/bin/encrypt.bat
@@ -2,7 +2,10 @@
 SETLOCAL
 
 CALL "%~dp0..\util\util.bat" checkJavaVersion
-IF "%ERRORLEVEL%" NEQ "0" GOTO :exit
+IF "%ERRORLEVEL%" NEQ "0" (
+    PAUSE
+    GOTO :exit
+)
 java -cp "%~dp0..\dataloader-%DATALOADER_VERSION%-uber.jar" com.salesforce.dataloader.security.EncryptionUtil %*
 
 :exit
