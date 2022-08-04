d6d62aeb200037cdc80d6efc9dde3c21738a3ed0
ashitsalesforce <ashit.patel@salesforce.com>
2022-08-03 21:41:41 -0700

    bat script cleanup
    - put double-quotes around a variable that may contain a space character in its value
    - code reuse - display banner in install.bat using the banner function in util.bat

--- release/win/bin/encrypt.bat
+++ release/win/bin/encrypt.bat
@@ -1,7 +1,7 @@
 @echo off
 SETLOCAL
 
-CALL %~dp0..\util\util.bat checkJavaVersion
+CALL "%~dp0..\util\util.bat" checkJavaVersion
 IF "%ERRORLEVEL%" NEQ "0" GOTO :exit
 java -cp "%~dp0..\dataloader-%DATALOADER_VERSION%-uber.jar" com.salesforce.dataloader.security.EncryptionUtil %*
 
--- release/win/bin/process.bat
+++ release/win/bin/process.bat
@@ -1,7 +1,7 @@
 @echo off
 SETLOCAL
 
-CALL %~dp0..\util\util.bat checkJavaVersion
+CALL "%~dp0..\util\util.bat" checkJavaVersion
 IF "%ERRORLEVEL%" NEQ "0" GOTO :exit
 java -jar "%~dp0..\dataloader-%DATALOADER_VERSION%-uber.jar" %* run.mode=batch
 
