0f7365cd9a0a1eb7ef5548869a7214a44d28837c
ashitsalesforce <ashit.patel@salesforce.com>
2023-03-14 21:43:55 -0700

    changes to utility scripts on windows and mac
    changes to utility scripts on windows and mac.

--- release/win/util/util.bat
+++ release/win/util/util.bat
@@ -1,7 +1,7 @@
 REM call the function specified in 1st param and return the errorlevel set by the function
 REM
 CALL :initVars
-CALL :%~1
+CALL %*
 EXIT /b %ERRORLEVEL%
 
 :initVars
@@ -36,6 +36,7 @@ EXIT /b %ERRORLEVEL%
     EXIT /b 0
 
 :checkJavaVersion
+    CALL :initVars
     echo Data Loader requires Java JRE %MIN_JAVA_VERSION% or later. Checking if it is installed...
 
     SET "PATH=%JAVA_HOME%\bin\;%PATH%;"
@@ -76,3 +77,24 @@ EXIT /b %ERRORLEVEL%
     echo ^<full path to the JRE installation folder^>
     echo.
     EXIT /b -1
+
+# Shortcut files have .lnk extension
+:CreateShortcut
+    powershell -Command "$WshShell = New-Object -comObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut(""""%~1""""); $Shortcut.WorkingDirectory = """"%~2""""; $Shortcut.TargetPath = """"%~2\dataloader.bat""""; $Shortcut.IconLocation = """"%~2\dataloader.ico""""; $Shortcut.WindowStyle=7; $Shortcut.Save()"
+    EXIT /b 0
+
+REM Note for label renaming - promptForShortcut sets "yesDestination" var based on naming convention.
+:createStartMenuShortcut
+    IF NOT EXIST "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Salesforce\" (
+        mkdir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Salesforce"
+    )
+    CALL :CreateShortcut "$Home\Desktop\Dataloader.lnk" %~1
+    move "%USERPROFILE%\Desktop\Dataloader.lnk" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Salesforce\Dataloader %DATALOADER_VERSION%.lnk" >nul
+    EXIT /b 0
+
+REM Note for label renaming - promptForShortcut sets "yesDestination" var based on naming convention.
+:createDesktopShortcut
+    CALL :CreateShortcut "$Home\Desktop\Dataloader.lnk" %~1
+    for /f "usebackq tokens=3*" %%D IN (`reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop`) do set DESKTOP_DIR=%%D
+    move "%USERPROFILE%\Desktop\Dataloader.lnk" "%DESKTOP_DIR%\Dataloader %DATALOADER_VERSION%.lnk" >nul
+    EXIT /b 0
