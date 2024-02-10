43bef832cae4e250faf0b9611bcc2499dbeef1e9
ashitsalesforce <ashit.patel@salesforce.com>
2024-02-09 19:00:56 -0800

    re-enable tests
    re-enable tests

--- src/test/java/com/salesforce/dataloader/process/CsvProcessTest.java
+++ src/test/java/com/salesforce/dataloader/process/CsvProcessTest.java
@@ -90,7 +90,7 @@ public class CsvProcessTest extends ProcessTestBase {
     /**
      * Tests the insert operation on Account - Positive test.
      */
-    @Ignore
+    @Test
     public void testInsertAccountCsv() throws Exception {
         Map<String, String> configMap = getTestConfig(OperationInfo.insert, false);
         if (isSettingEnabled(configMap, Config.BULK_API_ZIP_CONTENT)
@@ -132,7 +132,7 @@ public class CsvProcessTest extends ProcessTestBase {
     /**
      * Tests the insert operation on Account - Positive test.
      */
-    @Ignore
+    @Test
     public void testInsertTaskWithContactAsWhoCsv() throws Exception {
         Map<String, String> configMap = getTestConfig(OperationInfo.insert, false);
         if (isSettingEnabled(configMap, Config.BULK_API_ZIP_CONTENT)
@@ -165,7 +165,7 @@ public class CsvProcessTest extends ProcessTestBase {
     /**
      * Tests update operation with input coming from a CSV file. Relies on the id's in the CSV on being in the database
      */
-    @Ignore
+    @Test
     public void testUpdateAccountCsv() throws Exception {
         Map<String, String> configMap = getUpdateTestConfig(false, null, 100);
         if (isSettingEnabled(configMap, Config.BULK_API_ZIP_CONTENT)
@@ -181,7 +181,7 @@ public class CsvProcessTest extends ProcessTestBase {
     /**
      * Upsert the records from CSV file
      */
-    @Ignore
+    @Test
     public void testUpsertAccountCsv() throws Exception {
         Map<String, String> configMap = getUpdateTestConfig(true, DEFAULT_ACCOUNT_EXT_ID_FIELD, 50);
         if (isSettingEnabled(configMap, Config.BULK_API_ZIP_CONTENT)
@@ -206,7 +206,7 @@ public class CsvProcessTest extends ProcessTestBase {
      *                  when queried.
      * @throws Exception
      */
-    @Ignore
+    @Test
     public void testConstantMappingInCsv() throws Exception {
         Map<String, String> configMap = getTestConfig(OperationInfo.insert, false);
         if (isSettingEnabled(configMap, Config.BULK_API_ZIP_CONTENT)
@@ -244,7 +244,7 @@ public class CsvProcessTest extends ProcessTestBase {
      *
      * @throws Exception
      */
-    @Ignore
+    @Test
     public void testDescriptionAsConstantMappingInCsv() throws Exception {
         Map<String, String> configMap = getTestConfig(OperationInfo.insert, getTestDataDir()
                 + "/constantMappingInCsv.csv", false);
@@ -279,7 +279,7 @@ public class CsvProcessTest extends ProcessTestBase {
      *
      * @expectedResults Assert that the values retrieved for that field match those in the CSV file
      */
-    @Ignore
+    @Test
     public void testFieldAndConstantFieldClash()  throws Exception {
         Map<String, String> configMap = getTestConfig(OperationInfo.insert,
                 getTestDataDir() + "/constantMappingInCsvClashing.csv", false);
@@ -315,7 +315,7 @@ public class CsvProcessTest extends ProcessTestBase {
      *
      * @expectedResults Assert that the values retrieved for that field match those in the CSV file
      */
-    @Ignore
+    @Test
     public void testNullConstantAssignment()  throws Exception {
 
         /* Field assignments in .sdl are as follows:
@@ -378,7 +378,7 @@ public class CsvProcessTest extends ProcessTestBase {
     /**
      * Tests Upsert on foreign key for the records based on the CSV file
      */
-    @Ignore
+    @Test
     public void testUpsertFkAccountCsv() throws Exception {
         // manually inserts 100 accounts, then upserts specifying account parent for 50 accounts
         runUpsertProcess(getUpdateTestConfig(true, DEFAULT_ACCOUNT_EXT_ID_FIELD, 100), 0, 50);
@@ -387,7 +387,7 @@ public class CsvProcessTest extends ProcessTestBase {
     /**
      * Tests that Deleting the records based on a CSV file works
      */
-    @Ignore
+    @Test
     public void testDeleteAccountCsv() throws Exception {
         AccountIdTemplateListener listener = new AccountIdTemplateListener(100);
         String deleteFileName = convertTemplateToInput(baseName + "Template.csv", baseName + ".csv", listener);
@@ -422,7 +422,7 @@ public class CsvProcessTest extends ProcessTestBase {
         }
     }
 
-    @Ignore
+    @Test
     public void testCreateAttachment() throws Exception {
         // convert the template using the parent account id
         final String fileName = convertTemplateToInput(this.baseName + "Template.csv", this.baseName + ".csv",
@@ -452,7 +452,7 @@ public class CsvProcessTest extends ProcessTestBase {
      * @expectedResults Assert that all the records were inserted and that the constant value was mapped as well.
      *
      */
-    @Ignore
+    @Test
     public void testNonMappedFieldsPermittedInDLTransaction() throws Exception {
 
         final int numberOfRows = 4;
@@ -481,7 +481,7 @@ public class CsvProcessTest extends ProcessTestBase {
      * @expectedResults Assert that all the records were inserted and that the constant value was mapped as well.
      *
      */
-    @Ignore
+    @Test
     public void testHtmlFormattingInInsert() throws Exception {
         _doTestHtmlFormattingInInsert(true);
         _doTestHtmlFormattingInInsert(false);
@@ -577,7 +577,7 @@ public class CsvProcessTest extends ProcessTestBase {
      * @expectedResults Assert that the dates in Salesforce match up.
      *
      */
-    @Ignore
+    @Test
     public void testTimezoneNotTruncated() throws Exception {
 
         final int numberOfRows = 12;
@@ -617,7 +617,7 @@ public class CsvProcessTest extends ProcessTestBase {
      *
      * @throws Exception
      */
-    @Ignore
+    @Test
     public void testErrorsGeneratedOnInvalidDateMatching() throws Exception {
 
     	runTestErrorsGeneratedOnInvalidDateMatchWithOffset(0, 3,3);
@@ -630,12 +630,12 @@ public class CsvProcessTest extends ProcessTestBase {
      *
      * @throws Exception
      */
-    @Ignore
+    @Test
     public void testErrorsGeneratedOnInvalidDateMatchingWithOffset() throws Exception {
     	runTestErrorsGeneratedOnInvalidDateMatchWithOffset(2, 2, 2);
     }
     
-    @Ignore
+    @Test
     public void testOneToManySforceFieldsMappingInCsv() throws Exception {
         // The use case is as follows:
         // This company in this scenario only does business in the state of CA, therefore billing and shipping
@@ -668,7 +668,7 @@ public class CsvProcessTest extends ProcessTestBase {
         }
     }
     
-    @Ignore
+    @Test
     public void testEmptyFirstRowFieldValueInCsv() throws Exception {
         Map<String, String> argumentMap = getUpdateTestConfig(false, null, 2);
         if (isSettingEnabled(argumentMap, Config.BULK_API_ZIP_CONTENT)
