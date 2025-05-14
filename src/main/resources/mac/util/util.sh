cb72b393261922df8a011fa89a56edc4a865732e
ashitsalesforce <71143047+ashitsalesforce@users.noreply.github.com>
2025-05-14 10:10:55 -0700

    java version check in wrapper scripts
    having java version check in java code itself does not work because older versions of JRE complains about incompatible code. Switch back to having java version check in wrapper scripts.

--- src/main/resources/mac/util/util.sh
+++ src/main/resources/mac/util/util.sh
@@ -17,6 +17,8 @@ include() {
 
 initVars() {
     DATALOADER_VERSION="@@FULL_VERSION@@"
+    DATALOADER_SHORT_VERSION=$(echo "${DATALOADER_VERSION}" | cut -d'.' -f 1)
+    MIN_JAVA_VERSION=@@MIN_JAVA_VERSION@@
     
     # Source shell-specific files if they exist
     if [ "$SHELL_NAME" = "bash" ]; then
@@ -29,8 +31,36 @@ initVars() {
     include "${HOME}/.profile"
 }
 
-runDataLoader() {
+checkJavaVersion() {
     initVars
+    
+    echo "Data Loader requires Java JRE ${MIN_JAVA_VERSION} or later. Checking if it is installed..."
+    if [ -n "${DATALOADER_JAVA_HOME:-}" ]; then
+        JAVA_HOME="${DATALOADER_JAVA_HOME}"
+    fi
+    
+    PATH="${JAVA_HOME}/bin:${PATH}"
+    JAVA_VERSION=$(java -version 2>&1 | grep -i version | cut -d'"' -f 2 | cut -d'.' -f 1)
+    if [ -z "${JAVA_VERSION:-}" ]; then
+        echo "Did not find java command."
+        echo ""
+        exitWithJavaDownloadMessage
+    fi
+    if [ "${JAVA_VERSION}" -lt "${MIN_JAVA_VERSION}" ]; then
+        echo "Found Java JRE version ${JAVA_VERSION} whereas Data Loader requires Java JRE ${MIN_JAVA_VERSION} or later."
+        exitWithJavaDownloadMessage
+    fi
+}
+
+exitWithJavaDownloadMessage() {
+    echo "Java JRE ${MIN_JAVA_VERSION} or later is not installed or DATALOADER_JAVA_HOME environment variable is not set."
+    echo "For example, download and install Zulu JRE ${MIN_JAVA_VERSION} or later from here:"
+    echo "    https://www.azul.com/downloads/"
+    exit 1
+}
+
+runDataLoader() {
+    checkJavaVersion
     SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" >/dev/null 2>&1 && pwd -P )
     java -cp "${SCRIPT_DIR}/*" com.salesforce.dataloader.process.DataLoaderRunner "$@"
 }
